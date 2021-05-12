a4233c95285d06abba7ac9d24d1cacd468ed32df
Juan Pablo Díaz Correa <juanipis@gmail.com>
2021-05-11 20:57:24 -0500

    prueba 2

--- src/Ejercicios/HelloWorldNocturno.java
+++ src/Ejercicios/HelloWorldNocturno.java
@@ -3,5 +3,6 @@ package Ejercicios;
 public class HelloWorldNocturno {
   public static void main(String[] args) {
     System.out.println("¡¡¡¡QUE VIVA EL PARO!!!");
+    System.out.println("Otra vez");
   }
 }
