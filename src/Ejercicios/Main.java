b1d6fb6f7e3b33060674ca7900546d83c68d1b12
Juan Pablo <juanipis@gmail.com>
2021-10-13 13:21:29 -0500

    prueba vpn

--- src/Ejercicios/Main.java
+++ src/Ejercicios/Main.java
@@ -1,5 +1,6 @@
 package Ejercicios;
 
+
 public class Main {
   public static void main(String[] args) {
     Fechas fecha = new Fechas(12,4,2001);
@@ -7,5 +8,6 @@ public class Main {
     System.out.println("--------------");
     fecha.anadirDias(100000);
     System.out.println(fecha.toString());
+    System.out.println("Prueba")
   }
 }
